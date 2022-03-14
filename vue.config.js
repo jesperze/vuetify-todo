9be368cf186d43a83485738a3cfdfda5a99e4ec3
jesper.eriksson <jesperzeriksson@gmail.com>
2022-03-14 22:05:51 +0100

    gh-pages

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,6 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  publicPath: '/project name/',
+  publicPath: '\'/\'',
   transpileDependencies: [
     'vuetify'
   ]
